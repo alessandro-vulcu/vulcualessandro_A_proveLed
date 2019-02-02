f8367b805eda3e32f25730f3e18a2ea9b1977c81
Alessandro <alexvulcu21@gmail.com>
2019-02-02 11:27:15 +0100

    02/02/2019 - inserimento variabili

--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -3,7 +3,11 @@
   int ledverde1 = 4;
   int ledrosso2 = 3;
   int ledverde2 = 2;
-
+  int duecinquanta = 250;
+  int mille = 1000;
+  int millecinque = 1500;
+  int duemila = 2000;
+  
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledrosso1, OUTPUT);
@@ -17,52 +21,53 @@ void loop() {
   // put your main code here, to run repeatedly:
  digitalWrite(ledrosso1,HIGH);
  digitalWrite(ledverde2,HIGH);
- delay(5000);
+ delay(millecinque);
  digitalWrite(ledverde2,LOW);
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde2,HIGH); //1
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde2,LOW);
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde2,HIGH); //2
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde2,LOW);
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde2,HIGH); //3
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde2,LOW);
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde2,HIGH); //4
- delay(1000);
+ delay(duecinquanta);
+ digitalWrite(ledverde2, LOW);
  digitalWrite(ledrosso2,HIGH);
  
  digitalWrite(ledgiallo,HIGH);
- delay(5000);
+ delay(duemila);
  
  digitalWrite(ledrosso1,LOW);
- digitalWrite(ledrosso2,LOW);
  digitalWrite(ledgiallo,LOW);
  digitalWrite(ledverde1, HIGH);
- delay(5000);
+ delay(duemila);
  digitalWrite(ledverde1,LOW);
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde1,HIGH); //1
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde1,LOW);
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde1,HIGH); //2
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde1,LOW);
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde1,HIGH); //3
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde1,LOW);
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde1,HIGH); //4
- delay(1000);
+ delay(duecinquanta);
  digitalWrite(ledverde1,LOW);
  digitalWrite(ledgiallo,HIGH);
- delay(5000);
+ delay(duemila);
  digitalWrite(ledgiallo,LOW);
  digitalWrite(ledrosso1,HIGH);
+ digitalWrite(ledrosso2,LOW);
 }
