514b1ba02d587d8e820605b842ffe5ab006aaa26
Alessandro <alexvulcu21@gmail.com>
2019-02-09 20:34:33 +0100

    09/02/2019 - Conclusione del progetto. Sistemati problemi con la seriale e aggiunta richiesta tempo totale del semaforo.

--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -8,6 +8,8 @@
   int n1;
   int n2;
   int n3;
+  int n4;
+  int totale;
 void setup(){
   Serial.begin(9600);
   pinMode(ledrosso1, OUTPUT);
@@ -16,100 +18,125 @@ void setup(){
   pinMode(ledgiallo2, OUTPUT);
   pinMode(ledverde1, OUTPUT);
   pinMode(ledverde2, OUTPUT);
+
+  durata();
+  tempo1();
+  lampeggio();
+  nlampeggi();
+
+  }
   
-  
-    Serial.println("inserisci un tempo per il led rosso");
+  void durata(){
+    Serial.println("Inserisci la durata del semaforo in millisecondi");
     while(Serial.available() == 0){}
     n1 = Serial.readString().toInt();
-    Serial.println("Input 1 eseguito");
-
-    Serial.println("inserisci un secondo tempo per il led giallo");
+    Serial.println(n1);
+  }
+  void tempo1(){
+    Serial.println("Inserisci il tempo tra l'accensione di un led e l'altro");
     while(Serial.available() == 0){}
     n2 = Serial.readString().toInt();
-    Serial.println("Input 2 eseguito");
-
-    Serial.println("inserisci un terzo tempo per il lampeggio del led verde");
+    Serial.println(n2);
+  }
+  void lampeggio(){
+    Serial.println("Inserisci un tempo per il lampeggio del led verde");
     while(Serial.available() == 0){}
     n3 = Serial.readString().toInt();
-    Serial.println("Input 2 eseguito");
+    Serial.println(n3);
+  }
+  void nlampeggi(){
+    Serial.println("Inserisci il numero di lampeggi che i led verdi devono fare");
+    while(Serial.available() == 0){}
+    n4 = Serial.readString().toInt();
+    Serial.println(n4);
+    totale = n2 + (n3*n4);
+    errore();
   }
   
-  void coso(void){
-    
+  void errore(){
+    if (totale < n1){
+      Serial.println("ERRORE, la durata del semaforo è minore di quella che hai impostato");
+      durata();
+      tempo1();
+      lampeggio();
+      nlampeggi();
+      errore();
+    }
+    else if(totale > n1){
+      Serial.println("ERRORE, la durata del semaforo è maggiore di quella che hai impostato");
+      durata();
+      tempo1();
+      lampeggio();
+      nlampeggi();
+      errore();
+    }
+    else{
+      Serial.println("INIZIA!");
+    }
   }
+  
+ void fase1(){
+  digitalWrite(ledrosso2,LOW);
+  digitalWrite(ledgiallo1,LOW);
+  digitalWrite(ledgiallo2,LOW);
+  digitalWrite(ledrosso1,HIGH);
+  digitalWrite(ledverde2,HIGH);
+  delay(n2);
+ }
  
+ void fase2(){
+  for(int i = 0; i < n4; i++){
+    digitalWrite(ledverde2,LOW);
+    delay(n3);
+    digitalWrite(ledverde2,HIGH);
+    delay(n3);  
+  }
+ }
  
-  
-  
-  
-
-  
-
+ void fase3(){
+  digitalWrite(ledverde2,LOW);
+  digitalWrite(ledgiallo1,HIGH);
+  digitalWrite(ledgiallo2,HIGH);
+  delay(n2);
+ }
+ 
+ void fase4(){
+  digitalWrite(ledrosso1,LOW);
+  digitalWrite(ledgiallo1,LOW);
+  digitalWrite(ledgiallo2,LOW);
+  digitalWrite(ledrosso2,HIGH);
+  digitalWrite(ledverde1,HIGH);
+  delay(n2);
+ }
+ 
+ void fase5(){
+  for(int k = 0; k < n4; k++){
+    digitalWrite(ledverde1,LOW);
+    delay(n3);
+    digitalWrite(ledverde1,HIGH);
+    delay(n3);  
+  }
+ }
+ 
+ void fase6(){
+  digitalWrite(ledverde1,LOW);
+  digitalWrite(ledgiallo1,HIGH);
+  digitalWrite(ledgiallo2,HIGH);
+  delay(n2);
+ }
 
 void loop() {
   // put your main code here, to run repeatedly:
- digitalWrite(ledrosso1,HIGH);
- digitalWrite(ledverde2, HIGH);
- delay(n2);
- 
- digitalWrite(ledverde2, LOW);
- delay(n3);
- digitalWrite(ledverde2, HIGH);
- delay(n3);
- digitalWrite(ledverde2, LOW);
- delay(n3);
- digitalWrite(ledverde2, HIGH);
- delay(n3);
- digitalWrite(ledverde2, LOW);
- delay(n3);
- digitalWrite(ledverde2, HIGH);
- delay(n3);
- digitalWrite(ledverde2, LOW);
- delay(n3);
- digitalWrite(ledverde2, HIGH);
- delay(n3);
- digitalWrite(ledverde2, LOW);
- delay(n3);
- 
- digitalWrite(ledrosso1,HIGH);
- digitalWrite(ledgiallo1, HIGH);
- digitalWrite(ledgiallo2, HIGH);
- delay(n2);
- digitalWrite(ledrosso1,LOW);
- digitalWrite(ledgiallo1, LOW);
- digitalWrite(ledgiallo2, LOW);
- digitalWrite(ledrosso2, HIGH);
- digitalWrite(ledverde1, HIGH);
- delay(n2);
 
- digitalWrite(ledverde1, LOW);
- delay(n3);
- digitalWrite(ledverde1, HIGH);
- delay(n3);
- digitalWrite(ledverde1, LOW);
- delay(n3);
- digitalWrite(ledverde1, HIGH);
- delay(n3);
- digitalWrite(ledverde1, LOW);
- delay(n3);
- digitalWrite(ledverde1, HIGH);
- delay(n3);
- digitalWrite(ledverde1, LOW);
- delay(n3);
- digitalWrite(ledverde1, HIGH);
- delay(n3);
- digitalWrite(ledverde1, LOW);
- delay(n3);
- digitalWrite(ledrosso1,LOW);
- digitalWrite(ledrosso2,HIGH);
- digitalWrite(ledgiallo1, HIGH);
- digitalWrite(ledgiallo2, HIGH);
- delay(n2);
- digitalWrite(ledrosso2,LOW);
- digitalWrite(ledgiallo1,LOW);
- digitalWrite(ledgiallo2,LOW);
- digitalWrite(ledverde2,HIGH);
- digitalWrite(ledrosso1, HIGH);
+ 
+ fase1();
+ fase2();
+ fase3();
+ fase4();
+ fase5();
+ fase6();
+ 
+ 
  //FIN
  
  
